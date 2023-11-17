d7aaf6fb2a78e780ade341dca62982bff5675439
Hesham Tawfik <33587812+HeshamTawfik@users.noreply.github.com>
2023-11-17 14:44:59 +0200

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # this script contain all Azure CLI commands you used
 
-# Create an App Service in Azure. In this example the App Service is called jose-flaskpipelines and the resource group is called jose-udacity-project. The result take a few minutes:
-az webapp up -n jose-flaskpipelines -g jose-udacity-project
+# Create an App Service in Azure. In this example the App Service is called Continuous-Delivery-on-Azure and the resource group is called Continuous-Delivery-on-Azure-project. The result take a few minutes:
+az webapp up -n Continuous-Delivery-on-Azure -g Continuous-Delivery-on-Azure-project
 
 # view the logs:
-az webapp log tail -n jose-flaskpipelines -g jose-udacity-project
+az webapp log tail -n Continuous-Delivery-on-Azure -g Continuous-Delivery-on-Azure-project
 
