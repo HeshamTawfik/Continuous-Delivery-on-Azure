7c23bc1d7729bee0520707fcce5bd780378cf512
Hesham Tawfik <33587812+HeshamTawfik@users.noreply.github.com>
2023-11-19 14:00:24 +0200

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -10,5 +10,4 @@ class WebsiteTestUser(HttpUser):
     @task(2)
     def test2(self):
         self.client.post("http://localhost:5000/predict")
-        data = {"CHAS": {"0": 0}, "RM": {"0": 6.575}, "TAX": {"0": 296.0}, "PTRATIO": {"0": 15.3}, "B": {"0": 396.9},
-            "LSTAT": {"0": 4.98}}
+    
