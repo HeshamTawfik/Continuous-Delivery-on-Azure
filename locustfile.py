8b096a03da0fec17b13a216a2bf61cce01140174
Hesham Tawfik <33587812+HeshamTawfik@users.noreply.github.com>
2023-11-19 14:07:39 +0200

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -9,9 +9,18 @@ class WebsiteTestUser(HttpUser):
 
     @task(2)
     def test2(self):
-             
-            data = {"CHAS": {"0": 0}, "RM": {"0": 6.575}, "TAX": {"0": 296.0}, "PTRATIO": {"0": 15.3}, "B": {"0": 396.9},
-            "LSTAT": {"0": 4.98}}
-        
-        self.client.post('http://localhost:5000/predict', json=data)
+        data = {
+            "CHAS": {"0": 0},
+            "RM": {"0": 6.575},
+            "TAX": {"0": 296.0},
+            "PTRATIO": {"0": 15.3},
+            "B": {"0": 396.9},
+            "LSTAT": {"0": 4.98}
+        }
+
+        # Perform some HTTP request using the data
+        response = self.client.post("http://localhost:5000", json=data)
+
+        # You can print the response if needed
+        print(response.text)
         
